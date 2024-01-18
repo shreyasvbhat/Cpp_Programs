840ac37bb907250b24a98c69d807ed1997eb1065
SHREYAS V BHAT <117741480+shreyasvbhat@users.noreply.github.com>
2024-01-18 06:31:15 +0530

    Add files via upload

---
+++ simple_programs/reverse_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,r,sum = 0;
+    cin>>n;
+    while(n%10!=0){
+        r = n%10;
+        sum = sum*10+r;
+        n = n/10;
+    }
+    cout<<sum;
+
+    return 0;
+}
