dcc24b6ed4ea47dae746ba722b5eb3e4ad32e28f
SHREYAS V BHAT <117741480+shreyasvbhat@users.noreply.github.com>
2024-04-04 09:56:24 +0530

    Add files via upload

---
+++ Recursion/printNumber_recursion.cpp
@@ -0,0 +1,30 @@
+//Program to print numbers in sequence
+#include<iostream>
+using namespace std;
+
+void dec(int n){
+    if(n==1){
+        cout<<"1"<<endl;
+        return;
+    }
+    cout<<n<<endl;
+    dec(n-1);
+}
+
+void inc(int n){
+    if(n==1){
+        cout<<"1"<<endl;
+        return;
+    }
+    inc(n-1);
+    cout<<n<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number\n";
+    cin>>n;
+    dec(n);
+    inc(n);
+    return 0;
+}
